bf6db9512ab6c6ec0d0d09f500c3dcfaff987825
Saman Batool <samanbatool08@gmail.com>
2022-10-18 12:56:48 -0400

    conditional styling

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,3 +1,4 @@
+import './SeasonDisplay.css'
 import React from 'react'; 
 
 let seasonConfig = {
@@ -25,10 +26,10 @@ const SeasonDisplay = (props) => {
 
 
   return (
-    <div>
-      <i className={`${iconName} icon`} />
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive ${iconName} icon`} />
         <h1>{text}</h1>
-      <i className={`${iconName} icon`} />
+      <i className={`icon-right massive ${iconName} icon`} />
     </div>
 )};
 
