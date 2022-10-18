40105b70634ac8e5b19fb34c8fdd305f9b6e9421
Saman Batool <samanbatool08@gmail.com>
2022-10-18 12:07:34 -0400

    retreiving location

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
+
+
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => console.log(position.coords.latitude),
+    (error) => console.log(error.message)
+  );
+
+
   return (
     <div>
       Hi There! 
