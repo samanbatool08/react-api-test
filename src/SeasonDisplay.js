04febec5dcc24f2da98f9372f91298df2e6b3b7c
Saman Batool <samanbatool08@gmail.com>
2022-10-18 12:43:08 -0400

    showing icons

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -10,8 +10,16 @@ function getSeason(lat, month) {
 
 const SeasonDisplay = (props) => {
   let season = getSeason(props.lat, new Date().getMonth())
-  console.log(season)
-  return <div>Season Display</div>
-};
+  let text = season === 'winter' ? "Burrr, it's chilly" : "Let's hit the beach"
+  let icon = season === 'winter' ? 'snowflake' : 'sun'
+
+
+  return (
+    <div>
+      <i className={`${icon} icon`} />
+        <h1>{text}</h1>
+      <i className={`${icon} icon`} />
+    </div>
+)};
 
 export default SeasonDisplay; 
